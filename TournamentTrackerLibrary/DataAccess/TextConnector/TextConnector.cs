ddb7a3b5644d7bd78c4cc6f17be7136019ac7dd4
Emil Julian Costinea <ejc@arp-hansen.dk>
2023-07-05 20:21:00 +0200

    added TextConnector functions

---
+++ TournamentTrackerLibrary/DataAccess/TextConnector/TextConnector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using TournamentTrackerLibrary.Models;
+
+namespace TournamentTrackerLibrary.DataAccess.TextConnector
+{
+    public class TextConnector : IDataConnection
+    {
+        private const string PrizesFile = "PrizeModels.csv";
+        
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+            model.Id = 1;
+            return model;
+        }
+    }
+}
--- TournamentTrackerLibrary/GlobalConfig.cs
+++ TournamentTrackerLibrary/GlobalConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Configuration;
 using TournamentTrackerLibrary.DataAccess;
+using TournamentTrackerLibrary.DataAccess.TextConnector;
 
 namespace TournamentTrackerLibrary
 {
